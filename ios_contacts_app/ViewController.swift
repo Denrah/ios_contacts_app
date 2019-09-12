cbce37b045685c115e72b2394970fc07a71289e4
Denis Sharapov <dsharapov@kode-t.ru>
2019-09-12 19:05:28 +0700

    Initial Commit

---
+++ ios_contacts_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_contacts_app
+//
+//  Created by iOS Intern on 12/09/2019.
+//  Copyright © 2019 iOS Intern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
