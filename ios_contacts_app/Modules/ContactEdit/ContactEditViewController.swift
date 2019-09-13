c5906b55b54c503934d7421c55d0481ada356042
Denis Sharapov <dsharapov@kode-t.ru>
2019-09-13 12:44:18 +0700

    Add edit screen layout

--- ios_contacts_app/Modules/ContactEdit/ContactEditViewController.swift
+++ ios_contacts_app/Modules/ContactEdit/ContactEditViewController.swift
@@ -6,6 +6,8 @@
 import UIKit
 
 class ContactEditViewController: UIViewController {
+  @IBOutlet private weak var notesTextView: UITextView!
+  
   let viewModel: ContactEditViewModel
   
   // MARK: - ViewController setup
@@ -21,5 +23,33 @@ class ContactEditViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    notesTextView.delegate = self
+    setupFields()
+    setupNavigationBar()
+  }
+  
+  private func setupFields() {
+    notesTextView.textContainerInset = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+    notesTextView.textContainer.lineFragmentPadding = 0
+  }
+  
+  private func setupNavigationBar() {
+    navigationController?.navigationBar.barTintColor = UIColor.white
+    navigationController?.navigationBar.shadowImage = UIImage()
+    
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Canel", style: .plain, target: self, action: nil)
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: nil)
+  }
+}
+
+// MARK: - Resizing TextView on content change
+
+extension ContactEditViewController: UITextViewDelegate {
+  func textViewDidChange(_ textView: UITextView) {
+    let fixedWidth = textView.frame.size.width
+    let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+    textView.translatesAutoresizingMaskIntoConstraints = true
+    textView.frame.size = CGSize(width: fixedWidth, height: newSize.height)
+    textView.isScrollEnabled = false
   }
 }
